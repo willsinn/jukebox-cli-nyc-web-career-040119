aa1156c62df89628155e3ddb10deceff02b2de8c
w.h.sinn <w.h.sinn@gmail.com>
2019-03-26 19:58:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -46,3 +46,23 @@ end
 
 def run(songs)
   # help
+  input = ""
+  while input
+    puts "Please enter a command:"
+    input = gets.downcase.strip
+    case input
+    when 'list'
+      list(songs)
+    when 'play'
+      list(songs)
+      play(songs)
+    when 'help'
+      help
+    when 'exit'
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
+end
